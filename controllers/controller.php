807ad7173ed51d3af9b52d7f4b7c0e7aca4187c0
LagoDavo <LagoDavo@vodagola.brs>
2017-12-19 22:12:48 +0100

    merged with home part of project -> start coding

---
+++ controllers/controller.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikol
+ * Date: 21-Jul-17
+ * Time: 10:53 PM
+ */
+
+include('../db/classDataHandler.php');
+
+$res = new DataHandler();
+
+$q = "SELECT * FROM `test`";
+
+$result = $res->executeQuery($q);
+
+$re    = $result->fetchAll();
+
+foreach($re as $r) {
+    print_r($r);
+}
