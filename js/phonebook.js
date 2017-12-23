f4e46284b2db43a03ff6c4909c8698f963d0a76f
LagoDavo <LagoDavo@vodagola.brs>
2017-12-23 16:25:44 +0100

    js validation for contact form done, UI not done

--- js/phonebook.js
+++ js/phonebook.js
@@ -1,5 +1,13 @@
+//**********************************************************************************************************************
+//                           ___ _____  ______ _____ _      _____
+//                          |_  /  ___| |  ___|_   _| |    |  ___|
+//                            | \ `--.  | |_    | | | |    | |__
+//                            | |`--. \ |  _|   | | | |    |  __|
+//                        /\__/ /\__/ / | |    _| |_| |____| |___
+//                        \____/\____/  \_|    \___/\_____/\____/
+//**********************************************************************************************************************
 
-
+//
 function getContactInfo(){
     var info = {};
 
@@ -9,9 +17,15 @@ function getContactInfo(){
     info.email    = $('#email').val();
     info.phone    = $('#phone').val();
 
-    //console.log(info);
 
-    $.ajax({
+    console.log(validateContactInput(info.name, info.lastname, info.email, info.phone));
+
+    //console.log(validateName(info.name));
+    //console.log(validateName(info.lastname));
+    //console.log(validateEmail(info.email));
+    //console.log(validatePhone(info.phone));
+
+   /* $.ajax({
         url: "../controllers/controller.php",
         type: "POST",
         dataType: "JSON",
@@ -32,7 +46,67 @@ function getContactInfo(){
             }
             else{ alert("Your credentials are not correct!"); }
         }]
-    });
+    });*/
 
 }
+
+function validateContactInput(name, lastname, email, phone)
+{
+    if( validateName(name) && validateName(lastname) && validateEmail(email) && validatePhone(phone) ) return 1;
+    else return 0;
+}
+
+function validateName(name)
+{
+    var l    = name.length; // broj karaktera u stringu name, odnosno duzina imena
+    var ctrl = 0;
+    if(name == "" || l < 3) return 0;
+    for( var i = 0; i < l; i++ ){
+        if(isLetter(name[i])) ctrl++;
+    }
+
+    if(ctrl == i && i < 35) return 1;
+    else return 0;
+}
+
+function isLetter(str)
+{
+    return str.length === 1 && str.match(/[a-z]/i);
+}
+
+function isNumber(str)
+{
+    return str.length === 1 && str.match(/[0-9]/i);
+}
+
+function validateEmail(email)
+{
+    var monkey = 0, dots = 0, l = email.length;
+
+    for( var i = 0; i < l; i++ ){
+        if(email[i] == '@') monkey++;
+        if(email[i] == '.') dots++;
+    }
+    if(monkey > 0 && dots > 0) return 1;
+    else return 0;
+}
+
+function validatePhone(phone)
+{
+    var ctrl = 0, l = phone.length;
+    if(l < 3 || l > 25 || phone == "") return 0;
+    for( var i = 0; i < l; i++ ){
+        if(isNumber(phone[i])) ctrl++;
+    }
+    if(ctrl == i) return 1;
+    else return 0;
+}
+
+
+
+
+
+
+
+
 
